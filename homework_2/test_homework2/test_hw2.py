a7f1d1ddd0753c00c08ded866e9cabe98e1ed1e4
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2024-09-10 20:07:54 +0000

    changed import statement

--- homework_2/test_homework2/test_hw2.py
+++ homework_2/test_homework2/test_hw2.py
@@ -1,4 +1,4 @@
-from hw2_debugging import merge_sort
+from homework_2 import hw2_debugging
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -6,19 +6,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_sorted_list():
     arr = [1, 2, 3, 4, 5]
-    assert merge_sort(arr) == [1, 2, 3, 4, 5]
+    assert hw2_debugging.merge_sort(arr) == [1, 2, 3, 4, 5]
 
 
 def test_unsorted_list():
     arr = [5, 3, 8, 4, 2]
-    assert merge_sort(arr) == [2, 3, 4, 5, 8]
+    assert hw2_debugging.merge_sort(arr) == [2, 3, 4, 5, 8]
 
 
 def test_reverse_sorted_list():
     arr = [9, 7, 5, 3, 1]
-    assert merge_sort(arr) == [1, 3, 5, 7, 9]
+    assert hw2_debugging.merge_sort(arr) == [1, 3, 5, 7, 9]
 
 
 def test_list_with_duplicates():
     arr = [4, 2, 2, 8, 4]
-    assert merge_sort(arr) == [2, 2, 4, 4, 8]
+    assert hw2_debugging.merge_sort(arr) == [2, 2, 4, 4, 8]
