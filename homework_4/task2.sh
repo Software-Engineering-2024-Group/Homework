f9bec964b5597fde4f40eed48d5e917c72fc8e9d
Mugdha <mugdhajoshi225@gmail.com>
2024-10-24 00:21:33 -0400

    Modified task2 to break ties based on file size

--- homework_4/task2.sh
+++ homework_4/task2.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-# task2.sh
 
-# Define the directory where the dataset is located
-dataset_dir="dataset1"
-
-# a. 
-# 1. Find Files Containing “sample”
-grep -rl "sample" "$dataset_dir" | while read -r file; do
-    # 2. Count Occurrences of “CSC510”
-    count=$(grep -o "CSC510" "$file" | wc -l)
-    # 3. Filter Files with at Least 3 Occurrences
-    if [ "$count" -ge 3 ]; then
-        echo "$count $file"
-    fi
-done | sort -k1,1nr -k2,2 | uniq | awk '{ sub("file_", "filtered_", $2); print }' | tee filtered_files.txt
+grep -rl "sample" . | \
+xargs grep -l "CSC510" | \
+xargs grep -o "CSC510" | \
+uniq -c | \
+grep -E '^[[:space:]]*3|^[[:space:]]*[4-9]|^[[:space:]]*[1-9][0-9]' | \
+gawk '{print $1, $2}' | \
+gawk '{ "ls -l " substr($2, 1, index($2, ":")-1) " | gawk \047{print $5}\047" | getline size; print $1, size, substr($2, 1, index($2, ":")-1) }' | \
+sort -k1,1nr -k2,2nr | \
+gawk '{print $3}' | \
+sed 's/file/filtered/g'
